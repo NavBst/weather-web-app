89a397d4f8515200b4cabed6093e3865975ab428
Navneet Bisht <nick49805@gmail.com>
2025-04-19 20:05:31 +0530

    finishing show weathr

--- script.js
+++ script.js
@@ -15,8 +15,28 @@ document.addEventListener("DOMContentLoaded",()=>{
       const monYear = document.querySelector("main").firstElementChild.firstElementChild.firstElementChild;
       monYear.innerHTML = monthYear;
       monYear.nextElementSibling.innerHTML = fullDate;
-
 })
+
+// using current location...
+document.getElementById("currentLoc").addEventListener("click", ()=>{
+    let latitude = "";
+    let longitude = "";
+    
+        
+    navigator.geolocation.getCurrentPosition(async (position)=> {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        const cityUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=45a49cb5d9d6624a599cbac7a29e86f5`;
+        const city = await fetch(cityUrl).then(res=>res.json()) 
+        fetchWeather(city.name)
+        }, (error)=>{
+            console.log(error)
+        });
+        
+       
+})
+
+
 
 const fetchWeather = async (city) => { // fetching weather details...
     city = city === "" ? "delhi" : city;
@@ -41,7 +61,7 @@ const fetchWeather = async (city) => { // fetching weather details...
 
         const weatherData = await weatherResponse.json();
         const forecastData = await forecastResponse.json();
-        console.log({ weatherData, forecastData })
+        // console.log({ weatherData, forecastData })
         ShowDetails({ weatherData, forecastData });
     } catch (error) {
         showError(error.message || "An unexpected error occurred");
@@ -52,13 +72,16 @@ fetchWeather("");
 
 function showError(error) {
     console.error("Error: " + error);
+    let errorbox = document.getElementById("error");
+    // errorbox = 
+
 }
 
 
 // showing details.... 
 async function ShowDetails({ weatherData, forecastData }) {
     let country = await fetch(`https://restcountries.com/v3.1/alpha/${weatherData.sys.country || "delhi"}`).then(res => res.json()).then((res => res));
-    console.log(country)
+    // console.log(country)
     const city = document.getElementById("city");
     city.innerHTML = `${weatherData.name}`;
     city.nextElementSibling.innerHTML = `${country[0].name.common}`;
@@ -75,9 +98,6 @@ async function ShowDetails({ weatherData, forecastData }) {
     const sunsetTime = new Date((weatherData.sys.sunset +  weatherData.timezone) * 1000)
         .toLocaleTimeString("en-IN", { hour: "2-digit", minute: "2-digit", hour12: true });
 
-    console.log(`Sunrise: ${sunriseTime}`);
-    console.log(`Sunset: ${sunsetTime}`);
-
     const sunrise = document.getElementById("sunrise");
     sunrise.innerHTML = `Sunrise: ${sunriseTime}`;
     sunrise.nextElementSibling.innerHTML = `Sunset: ${sunsetTime}`;
@@ -86,7 +106,6 @@ async function ShowDetails({ weatherData, forecastData }) {
     let forecast = document.getElementById("add_row");
     forecast.innerHTML = " ";
     let count = 0;
-    console.log(forecastData);
 
     const filteredDailyForecasts = forecastData.list.filter(entry => {
         return entry.dt_txt.includes("12:00:00"); // or "15:00:00" if you prefer afternoon
@@ -102,6 +121,17 @@ async function ShowDetails({ weatherData, forecastData }) {
                     <td>${value.clouds.all}</td>
                 </tr>`;
     })
+
+    const wind = document.getElementById("today");
+    wind.lastElementChild.innerHTML = weatherData.wind.speed + " m/s";
+
+    let  humi = wind.nextElementSibling;
+    humi.lastElementChild.innerHTML = weatherData.main.humidity+"%";
+
+    let pressure = humi.nextElementSibling;
+    pressure.lastElementChild.innerHTML = weatherData.main.pressure + " hPa";
+
+    pressure.nextElementSibling.lastElementChild.innerHTML = weatherData.visibility/1000 + " Km";
 }
 
 search.addEventListener("keypress", (e) => {
@@ -111,3 +141,6 @@ search.addEventListener("keypress", (e) => {
         fetchWeather(city);
     }
 })
+
+
+
